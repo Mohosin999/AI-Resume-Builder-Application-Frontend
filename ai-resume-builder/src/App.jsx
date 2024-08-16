21f513d8b951d57ef63f3018f58609001e76c6d0
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-08-16 17:29:54 +0600

    first commit

---
+++ ai-resume-builder/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>23min</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
