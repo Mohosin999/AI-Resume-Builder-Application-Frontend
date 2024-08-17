a8cf4ebdd3642773deb5aa09dcaafd35dfeccf09
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-08-17 10:51:54 +0600

    header section implemented

--- ai-resume-builder/src/components/custom/Header.jsx
+++ ai-resume-builder/src/components/custom/Header.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
+import { UserButton, useUser } from "@clerk/clerk-react";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className="p-3 py-5 flex justify-between shadow-md">
       <img src="./logo.svg" width={100} height={100} />
 
-      <Button>Get Started</Button>
+      {isSignedIn ? (
+        <div className="flex gap-2 items-center">
+          <Link to={"/dashboard"}>
+            <Button variant="outline">Dashboard</Button>
+          </Link>
+          <UserButton />
+        </div>
+      ) : (
+        <Link to={"/auth/sign-in"}>
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 };
--- ai-resume-builder/src/home/index.jsx
+++ ai-resume-builder/src/home/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-// import { UserButton } from "@clerk/clerk-react";
 import Header from "../components/custom/Header";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <p>User Information is loading...</p>
+      User Information is loading...
     </div>
   );
 };
