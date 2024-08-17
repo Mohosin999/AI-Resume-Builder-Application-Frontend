4b12de323979572d0b43bcef3df94f7276475aec
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-08-17 06:58:32 +0600

    update main.jsx page adding router

--- ai-resume-builder/src/main.jsx
+++ ai-resume-builder/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import SignInPage from "./auth/sign-in/index.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
