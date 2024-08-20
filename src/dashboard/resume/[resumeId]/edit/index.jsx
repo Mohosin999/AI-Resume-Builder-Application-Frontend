d39f4ecee7aa37d36bbef33a44d70b858d4b12cd
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-08-20 07:59:05 +0600

    working with dynamic route to edit resume

---
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditResume = () => {
+  return <div>EditResume</div>;
+};
+
+export default EditResume;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import SignInPage from "./auth/sign-in/index.jsx";
 import Home from "./home/index.jsx";
 import Dashboard from "./dashboard/index.jsx";
+import EditResume from "./dashboard/resume/[resumeId]/edit/index.jsx";
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "/dashboard",
         element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/resume/:resumeId/edit",
+        element: <EditResume />,
       },
     ],
   },
