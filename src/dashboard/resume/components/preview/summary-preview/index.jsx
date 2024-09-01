4e50362f3b0601445d38cd261ef979e3111aa088
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-09-01 17:32:00 +0600

    refactor: summary section refactored

--- src/dashboard/resume/components/preview/summary-preview/index.jsx
+++ src/dashboard/resume/components/preview/summary-preview/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const SummaryPreview = ({ resumeInfo }) => {
-  return <p className="text-xs">{resumeInfo?.attributes?.summary}</p>;
+  return (
+    <div className="my-6">
+      {/* Heading */}
+      <h1
+        className="font-bold text-sm text-center"
+        style={{ color: resumeInfo?.attributes?.themeColor }}
+      >
+        Summary
+      </h1>
+      {/* Horizontal line */}
+      <hr
+        className="my-1"
+        style={{ borderColor: resumeInfo?.attributes?.themeColor }}
+      />
+
+      {/* Summary Details */}
+      <p className="text-xs">{resumeInfo?.attributes?.summary}</p>
+    </div>
+  );
 };
 
 export default SummaryPreview;
