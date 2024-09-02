30398b7bb7d8f7d0f1c175d7995182c04b439cea
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-09-02 11:24:18 +0600

    refactor: personal information section refactored

--- src/dashboard/resume/components/preview/personal-details-preview/index.jsx
+++ src/dashboard/resume/components/preview/personal-details-preview/index.jsx
@@ -1,61 +1,3 @@
-// import React from "react";
-
-// const PersonalDetailsPreview = ({ resumeInfo }) => {
-//   return (
-//     <div>
-//       {/* Name */}
-//       <h2
-//         className="font-bold text-xl text-center"
-//         style={{
-//           color: resumeInfo?.attributes?.themeColor,
-//         }}
-//       >
-//         {resumeInfo?.attributes?.firstName} {resumeInfo?.attributes?.lastName}
-//       </h2>
-//       {/* Job title */}
-//       <h2 className="font-medium text-sm text-center">
-//         {resumeInfo?.attributes?.jobTitle}
-//       </h2>
-//       {/* Address */}
-//       <h2
-//         className="font-normal text-xs text-center"
-//         style={{
-//           color: resumeInfo?.attributes?.themeColor,
-//         }}
-//       >
-//         {resumeInfo?.attributes?.address}
-//       </h2>
-//       {/* Phone & Email */}
-//       <div className="flex justify-between">
-//         <h2
-//           className="font-normal text-xs"
-//           style={{
-//             color: resumeInfo?.attributes?.themeColor,
-//           }}
-//         >
-//           {resumeInfo?.attributes?.phone}
-//         </h2>
-//         <h2
-//           className="font-normal text-xs"
-//           style={{
-//             color: resumeInfo?.attributes?.themeColor,
-//           }}
-//         >
-//           {resumeInfo?.attributes?.email}
-//         </h2>
-//       </div>
-
-//       {/* Horizontal line */}
-//       <hr
-//         className="border-[1.5px] my-2"
-//         style={{ borderColor: resumeInfo?.attributes?.themeColor }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default PersonalDetailsPreview;
-
 import React from "react";
 
 const PersonalDetailsPreview = ({ resumeInfo }) => {
@@ -85,15 +27,6 @@ const PersonalDetailsPreview = ({ resumeInfo }) => {
        * =========================================================
        */}
       <div className="flex justify-center items-center space-x-2">
-        {/* <h2
-          className="font-normal text-xs"
-          style={{
-            color: resumeInfo?.attributes?.themeColor,
-          }}
-        >
-          {resumeInfo?.attributes?.phone}
-        </h2> */}
-
         {/* Email */}
         <h2
           className="font-normal text-xs"
@@ -104,7 +37,11 @@ const PersonalDetailsPreview = ({ resumeInfo }) => {
           {resumeInfo?.attributes?.email}
         </h2>
         {/* Bullet Point */}
-        <span className="text-xs font-bold">•</span>
+        {resumeInfo?.attributes?.email &&
+          resumeInfo?.attributes?.socialLink && (
+            <span className="text-xs font-bold">•</span>
+          )}
+
         {/* Social Link */}
         <h2
           className="font-normal text-xs"
@@ -115,7 +52,11 @@ const PersonalDetailsPreview = ({ resumeInfo }) => {
           {resumeInfo?.attributes?.socialLink}
         </h2>
         {/* Bullet Point */}
-        <span className="text-xs font-bold">•</span>
+        {resumeInfo?.attributes?.socialLink &&
+          resumeInfo?.attributes?.address && (
+            <span className="text-xs font-bold">•</span>
+          )}
+
         {/* Address */}
         <h2
           className="font-normal text-xs text-center"
