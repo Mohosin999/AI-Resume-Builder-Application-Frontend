fba324076bc393d632c4dc1803e8b5f598ff4df6
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-09-27 08:54:23 +0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { useUser } from "@clerk/clerk-react";
 import { Navigate, Outlet } from "react-router-dom";
 import Header from "./components/header";
 import { Toaster } from "./components/ui/sonner";
-import { useState } from "react";
 
 function App() {
-  const [isTrue, setIsTrue] = useState();
   /**
    * The isLoaded is a boolean value that indicates whether the user
    * data has been fully loaded or not.
    */
-  const { user, isLoaded, isSignedIn } = useUser();
+  const { isLoaded, isSignedIn } = useUser();
 
   /**
    * If the user is not authenticated and all user data is fully loaded,
